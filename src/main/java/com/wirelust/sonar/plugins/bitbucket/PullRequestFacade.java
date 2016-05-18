942e73fcfc46636e0a959be6ecb5cfa313d47f67
Terrence Curran <tcurran@approachingpi.com>
2016-05-18 18:28:04 -0400

    sonarqube defects

--- src/main/java/com/wirelust/sonar/plugins/bitbucket/PullRequestFacade.java
+++ src/main/java/com/wirelust/sonar/plugins/bitbucket/PullRequestFacade.java
@@ -106,13 +106,16 @@ public class PullRequestFacade implements BatchComponent {
       LOGGER.error("unable to register jax-rs providers", e);
     }
 
-    if (config.login() == null || config.login().isEmpty()) {
+    String login = config.login();
+    if (login == null || login.isEmpty()) {
       throw new IllegalStateException(BitBucketPlugin.BITBUCKET_LOGIN + " cannot be null");
     }
 
-    if (config.password() == null || config.password().isEmpty()) {
+    String password = config.password();
+    if (password == null || password.isEmpty()) {
       throw new IllegalStateException(BitBucketPlugin.BITBUCKET_PASSWORD + " cannot be null");
     }
+
 
     if (config.repository() == null || config.repository().isEmpty()) {
       throw new IllegalStateException(BitBucketPlugin.BITBUCKET_REPO + " cannot be null");
